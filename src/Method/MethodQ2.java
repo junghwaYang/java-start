ab11596475f47e54ea0544e42702a8e2dc7a0822
tare <siltarre@gmail.com>
2025-06-14 09:34:13 +0900

    메서드 제시된 코드 리팩토링 하기

---
+++ src/Method/MethodQ2.java
@@ -0,0 +1,33 @@
+package Method;
+
+// 기존 코드 리팩토링 하기
+public class MethodQ2 {
+
+//    public static void main(String[] args) {
+//        String message = 'Hello, world!';
+//
+//        for (int i = 0; i < 3; i++) {
+//            System.out.println(message);
+//        }
+//
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(message);
+//        }
+//
+//        for (int i = 0; i < 7; i++) {
+//            System.out.println(message);
+//        }
+//    }
+
+    public static void main(String[] args) {
+        print("Hello", 3);
+        print("Hello", 5);
+        print("Hello", 7);
+    }
+
+    public static void print(String msg, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(msg);
+        }
+    }
+}
